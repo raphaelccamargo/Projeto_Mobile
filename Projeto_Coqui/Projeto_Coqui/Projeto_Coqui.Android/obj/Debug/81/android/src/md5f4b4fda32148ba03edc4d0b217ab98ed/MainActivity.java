eb67c26dc1ee9d89b1649f9162fc99cb024e4828
raphaelccamargo <36749686+raphaelccamargo@users.noreply.github.com>
2020-05-15 20:53:56 -0300

    Projeto_Mobile

---
+++ Projeto_Coqui/Projeto_Coqui/Projeto_Coqui.Android/obj/Debug/81/android/src/md5f4b4fda32148ba03edc4d0b217ab98ed/MainActivity.java
@@ -0,0 +1,47 @@
+package md5f4b4fda32148ba03edc4d0b217ab98ed;
+
+
+public class MainActivity
+	extends md51558244f76c53b6aeda52c8a337f2c37.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("Projeto_Coqui.Droid.MainActivity, Projeto_Coqui.Android", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("Projeto_Coqui.Droid.MainActivity, Projeto_Coqui.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ Projeto_Coqui/Projeto_Coqui/Projeto_Coqui/MainPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace Projeto_Coqui
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public static async Task<string> Localizacao()
+        {
+            var location = await Geolocation.GetLastKnownLocationAsync();
+            string retorno = "Não foi possível pegar a localização";
+
+            if (location != null)
+            {
+                retorno = $"Latitude: {location.Latitude}, Longitude: {location.Longitude}, Altitude: {location.Altitude}";
+            }
+
+            return retorno;
+        }
+
+        private void btnGPS_Clicked(object sender, EventArgs e)
+        {
+            var location = Localizacao().Result;
+            lblGPS.Text = location;
+        }
+
+        private void btnConexao_Clicked(object sender, EventArgs e)
+        {
+            if (Connectivity.NetworkAccess == NetworkAccess.Internet)
+                lblConexao.Text = "Com acesso a internet";
+            else
+                lblConexao.Text = "Sem acesso a internet";
+        }
+    }
+}
